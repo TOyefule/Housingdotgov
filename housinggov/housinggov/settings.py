045d55ac57ddedfa2ff125dc4ba57b24697e7103
pnicewicz421 <peter.nicewicz@gmail.com>
2019-01-24 16:52:45 -0500

    Add files via upload

--- housinggov/housinggov/settings.py
+++ housinggov/housinggov/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'housing.apps.HousingConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
