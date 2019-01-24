9d20ba84291a5c5552e9c8f28a6265388780add2
pnicewicz421 <peter.nicewicz@gmail.com>
2019-01-24 17:04:42 -0500

    Update settings.py

--- housinggov/housinggov/settings.py
+++ housinggov/housinggov/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'housing.apps.HousingConfig'
+    'housing.apps.HousingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
