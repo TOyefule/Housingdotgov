7294a6ef58dc8c0dc4084c18f920b6f615269d8d
pnicewicz421 <peter.nicewicz@gmail.com>
2019-01-24 17:04:20 -0500

    Update models.py

--- housinggov/housing/models.py
+++ housinggov/housing/models.py
@@ -7,50 +7,28 @@ class Applicant(models.Model):
 	last_name = models.CharField(max_length=200)
 	DOB = models.DateTimeField('DOB')
 
-	current_city_or_county = CharField(max_length=50)
-	current_state = CharField(max_length=2)
-	current_zip = CharField(max_length=9)
-	current_country = CharField(max_length=15)
+	current_city_or_county = models.CharField(max_length=50)
+	current_state = models.CharField(max_length=2)
+	current_zip = models.CharField(max_length=9)
+	current_country = models.CharField(max_length=15)
 
-	veteran_status = BooleanField()
+	veteran_status = models.BooleanField()
 
-	hcv_recipient = BooleanField()
-	VASH = BooleanField()
-	other_subsidy = BooleanField()
+	hcv_recipient = models.BooleanField()
+	VASH = models.BooleanField()
+	other_subsidy = models.BooleanField()
 
 	# housing grant to resident to landlord
 
 	differently_abled = models.BooleanField()
-	hearing_impaired = model.BooleanField()
-	deaf = model.BooleanField()
-	visually_impaired = model.BooleanField()
-	blind = model.BooleanField()
-	physically_impaired = model.BooleanField()
-	wheelchair = model.BooleanField()
-	walker = model.BooleanField()
-	intellectually_impaired = model.BooleanField()
-
-class Waitlist(models.Model):
-	# Property
-	prop = models.ForeignKey(Property, on_delete=models.CASCADE)
-	# Applicant
-	applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE) 
-
-class Landlord(models.Model):
-	prop = models.ForeignKey(Property, on_delete=models.CASCADE)
-	first_name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
-	company_name = models.CharField(max_length=200)
-
-	landlord_address1 = models.CharField(max_length=200)
-	landlord_address2 = models.CharField(max_length=200)
-	landlord_state = models.CharField(max_length=2)
-	landlord_zip = models.CharField(max_length=9)
-
-
-	website = models.CharField(max_length=200)
-
-	nonprofit_flag = models.BooleanField()
+	hearing_impaired = models.BooleanField()
+	deaf = models.BooleanField()
+	visually_impaired = models.BooleanField()
+	blind = models.BooleanField()
+	physically_impaired = models.BooleanField()
+	wheelchair = models.BooleanField()
+	walker = models.BooleanField()
+	intellectually_impaired = models.BooleanField()
 
 class Property(models.Model):
 	property_name = models.CharField(max_length=200)
@@ -71,3 +49,27 @@ class Property(models.Model):
 	has_accessible_unit = models.BooleanField()
 
 	full_waitlist = models.BooleanField()
+
+class Landlord(models.Model):
+	prop = models.ForeignKey(Property, on_delete=models.CASCADE)
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	company_name = models.CharField(max_length=200)
+
+	landlord_address1 = models.CharField(max_length=200)
+	landlord_address2 = models.CharField(max_length=200)
+	landlord_state = models.CharField(max_length=2)
+	landlord_zip = models.CharField(max_length=9)
+
+
+	website = models.CharField(max_length=200)
+
+	nonprofit_flag = models.BooleanField()
+
+
+
+class Waitlist(models.Model):
+	# Property
+	prop = models.ForeignKey(Property, on_delete=models.CASCADE)
+	# Applicant
+	applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE) 
